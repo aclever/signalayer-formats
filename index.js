d25705a94f2ad0694931ddffb7ce59650265118c
Alex Clever <alex.clever@signalayer.com>
2019-02-18 18:59:15 +0200

    initial files

---
+++ index.js
@@ -0,0 +1,25 @@
+var formatContainer = document.createElement('div');
+var formatText = document.createElement('div');
+var formatIMG = document.createElement("img");
+
+formatContainer.id = 'formatContainer';
+formatContainer.className = 'hidden';
+
+formatText.id = 'formatText';
+formatText.className = 'format-text';
+formatIMG.id = 'formatIMG';
+
+formatIMG.src = "ahorse.png";
+formatText.innerText = 'Join me in death';
+
+
+
+
+formatContainer.appendChild(formatIMG);
+formatContainer.appendChild(formatText);
+document.body.appendChild(formatContainer);
+
+
+function runAnimation1(){
+    formatContainer.className = 'format-animation-show';
+}
