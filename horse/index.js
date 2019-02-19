3709fe408a7033831abf1de8ef0fb874ffaa8df4
Alex Clever <alex.clever@signalayer.com>
2019-02-19 19:04:55 +0200

    added animations

--- horse/index.js
+++ horse/index.js
@@ -10,9 +10,23 @@ formatText.className = 'format-text';
 formatIMG.id = 'formatIMG';
 
 formatIMG.src = "ahorse.png";
-formatText.innerText = 'Join me in death';
+formatText.innerHTML = '<section class="container">\n' +
+    '  <h1 class="title">\n' +
+    '    <span>Hi, nice</span>\n' +
+    '    <span>to see</span>\n' +
+    '    <span>you here</span>\n' +
+    '  </h1>\n' +
 
-
+    '<button>$100 NOW</button>'
+    // '  \n' +
+    // '  <h2 class="title">\n' +
+    // '    <span>This is</span>\n' +
+    // '    <span>a long</span>\n' +
+    // '    <span>sub title</span>\n' +
+    // '    \n' +
+    // '    \n' +
+    // '  </h2>\n' +
+    '</section>';
 
 
 formatContainer.appendChild(formatIMG);
@@ -20,7 +34,34 @@ formatContainer.appendChild(formatText);
 document.body.appendChild(formatContainer);
 
 
-function runAnimation1(){
-    console.log('animation');
-    formatContainer.className = 'format-animation-show';
+function runAnimation1() {
+    setTimeout(function () {
+        formatContainer.className = 'format-animation-show';
+
+        setTimeout(function () {
+            formatContainer.className = 'hidden';
+            runAnimation2();
+        }, 3000)
+    }, 3000);
+}
+
+function runAnimation2() {
+
+    formatContainer.className = 'format-animation-show-2';
+
+    setTimeout(function () {
+        formatContainer.className = 'hidden';
+
+        runAnimation3();
+    }, 3000)
+
+}
+
+
+function runAnimation3() {
+    formatContainer.className = 'format-animation-show-3';
+
+    setTimeout(function () {
+        formatText.className = 'format-text format-text-show';
+    }, 2000)
 }
