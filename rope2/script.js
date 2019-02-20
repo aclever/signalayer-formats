03a03b735853b5c18e1316cd057c91c0bc0f9211
Alex Clever <alex.clever@signalayer.com>
2019-02-20 19:32:30 +0200

    added rope

---
+++ rope2/script.js
@@ -0,0 +1,26 @@
+var activeClass = 'chain';
+
+var tm1 = setTimeout(function () {
+    Array.prototype.forEach.call(document.getElementsByClassName('chain'), function (ch) {
+        ch.className = ch.className.replace('chain', 'chain-interval')
+    })
+    //periodical-n-swing /// new class name
+
+
+    activeClass = 'chain-interval';
+
+}, 5000);
+
+var button = document.getElementById('activePull');
+button.addEventListener('click', function () {
+    clearTimeout(tm1);
+
+    var classType = activeClass;
+
+
+    var nextElem = document.getElementsByClassName(classType);
+
+    Array.prototype.forEach.call(nextElem, function (ch) {
+        ch.className = ch.className.replace(classType, 'chain-hide');
+    })
+});
