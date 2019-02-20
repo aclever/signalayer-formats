04cd716434085412db43bc7407ce7a5fa90b0c00
Alex Clever <alex.clever@signalayer.com>
2019-02-20 12:16:07 +0200

    added banner

--- banner1/script.js
+++ banner1/script.js
@@ -3,10 +3,15 @@ img.src = 'rino.jpeg';
 
 var imgContainer = document.createElement('div');
 
-imgContainer.innerHTML = '<h3>Try Signalayer free for 14 days</h3>' +
-    '<p>Start creating online marketing campaigns for your business with Signalayer’s 14-day free trial. No commitment.</p>'
-    + '<button>Please NOW</button>';
+
 imgContainer.className = 'img-container';
+
+
+setTimeout(function(){
+    imgContainer.innerHTML = '<h3>Try Signalayer free for 14 days</h3>' +
+        '<p>Start creating online marketing campaigns for your business with Signalayer’s 14-day free trial. No commitment.</p>'
+        + '<button>Please NOW</button>';
+}, 2400);
 
 var placeForBanner = document.getElementById('placeForBanner');
 
